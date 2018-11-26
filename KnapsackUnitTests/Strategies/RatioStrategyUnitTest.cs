f1481a4a1566f6cc70799f23679e938cb7584cf5
OPTOTUNE\tomas.chladek <tomas.chladek@optotune.com>
2018-11-26 20:19:02 +0100

    Unit tests for APR KNAPSACK

--- KnapsackUnitTests/Strategies/RatioStrategyUnitTest.cs
+++ KnapsackUnitTests/Strategies/RatioStrategyUnitTest.cs
@@ -1,4 +1,6 @@
-﻿using KnapsackSdk.Strategies;
+﻿using System.Collections.Generic;
+using KnapsackSdk.Dtos;
+using KnapsackSdk.Strategies;
 using NUnit.Framework;
 
 namespace KnapsackUnitTests.Strategies
@@ -18,8 +20,31 @@ namespace KnapsackUnitTests.Strategies
         public sealed class RatioStrategyUnitTestShould : RatioStrategyUnitTest
         {
             [Test]
-            public void ReturnCorrectResult()
+            public void ReturnMostValuableItem()
             {
+                var items = new List<ItemDto>
+                {
+                    new ItemDto(51,52),
+                    new ItemDto(90,60),
+                };
+                var definition = new DefinitionDto(1,100,items);
+                var result = Strategy.Compute(definition);
+                Assert.AreEqual(items[1].Price, result.Item1.Price);
+            }
+
+            [Test]
+            public void ReturnSumOfSmallItems()
+            {
+                var items = new List<ItemDto>
+                {
+                    new ItemDto(51,52),
+                    new ItemDto(50,50),
+                    new ItemDto(30,30),
+                    new ItemDto(20,19),
+                };
+                var definition = new DefinitionDto(1,100,items);
+                var result = Strategy.Compute(definition);
+                Assert.AreEqual(items[0].Price+items[2].Price,result.Item1.Price);
             }
         }
     }
